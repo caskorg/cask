3e3691f3a9f726032cfc8a7f532af940ec479d82
Paul Grigoras <paul.grigoras90@gmail.com>
2014-11-10 22:51:39 +0000

    Test values as expected.

--- sparsegrind/test/test_main.py
+++ sparsegrind/test/test_main.py
@@ -1,6 +1,8 @@
 from sparsegrind.sparsegrindio import io
 import os
 import unittest
+import numpy as np
+from numpy import testing
 
 
 def path_to(file):
@@ -15,6 +17,9 @@ class TestMain(unittest.TestCase):
     def test_main(self):
         self.assertEquals(len(self.csr_matrix.data), 8)
         self.assertEquals(len(self.csr_matrix.indptr), 5 + 1)
+        testing.assert_allclose(
+            self.csr_matrix.data,
+            np.array([1, 6, 10.5, 0.015, 250.5, -280, 33.32, 12]))
 
 
 if __name__ == '__main__':
