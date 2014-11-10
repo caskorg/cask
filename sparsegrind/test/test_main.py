ff652d3bedbc3012266b894dcc03ed57cbc9ff95
Paul Grigoras <paul.grigoras90@gmail.com>
2014-11-10 23:07:56 +0000

    Check timeline read.

--- sparsegrind/test/test_main.py
+++ sparsegrind/test/test_main.py
@@ -13,14 +13,28 @@ class TestMain(unittest.TestCase):
 
     def setUp(self):
         self.csr_matrix = io.read_matrix_market(path_to("small.in"))
+        self.timeline = io.read_matlab_matrix_timeline(
+            path_to("small_matlabtl.in"),
+            matrixsize=2
+        )
 
-    def test_main(self):
+    def testCsrRead(self):
         self.assertEquals(len(self.csr_matrix.data), 8)
         self.assertEquals(len(self.csr_matrix.indptr), 5 + 1)
         testing.assert_allclose(
             self.csr_matrix.data,
             np.array([1, 6, 10.5, 0.015, 250.5, -280, 33.32, 12]))
 
+    def testTimelineRead(self):
+        cmplx = self.timeline[0]
+        self.assertEquals(len(cmplx), 2)
+        testing.assert_allclose(
+            cmplx[0].data,
+            np.array([2+1j, 1+3j, 3, 4]))
+        testing.assert_allclose(
+            cmplx[1].data,
+            np.array([2.1+1j, 1.1+3j, 3.1, 4.1]))
+        
 
 if __name__ == '__main__':
     unittest.main()
