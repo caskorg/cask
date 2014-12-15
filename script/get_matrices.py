cfdc77372e7353916ffcd8eb454ba4dc817c2b01
Paul Grigoras <paul.grigoras90@gmail.com>
2014-12-15 13:02:30 +0000

    Support value type. Add a dry run option.

--- script/get_matrices.py
+++ script/get_matrices.py
@@ -2,18 +2,19 @@ import os
 import shutil
 import sys
 import wget
+import argparse
 
 from collections import namedtuple
 from subprocess import call
 from HTMLParser import HTMLParser
 
-Matrix=namedtuple('Matrix', ['group', 'name', 'id', 'rows', 'cols', 'nonZeros', 'file'])
+Matrix=namedtuple('Matrix', ['group', 'name', 'id', 'rows', 'cols', 'nonZeros', 'file', 'valuetype'])
 
 benchmarks=[
     ('matrix-vector', 'armadillo.cpp', 'g++ armadillo.cpp -o armadillo -O1 -larmadillo', 'armadillo')]
 
 # Max number of matrices to fetch
-MATRIX_LIMIT=1000
+MATRIX_LIMIT=460
 
 # What groups to fetch matrices from
 MATRIX_GROUPS = []
@@ -22,9 +23,9 @@ MATRIX_GROUPS = []
 MATRIX_NAMES = []
 
 # Max row and col sizes to fetch. Set to None if no limit is required
-MAX_ROW_SIZE = None
-MAX_COL_SIZE = None
-MAX_NON_ZEROS = 200000
+MAX_NON_ZEROS = 1E9
+
+ValueType = 'real' # 'real', 'integer', 'complex', 'binary'
 
 # The range of legal non-zero values
 NonZeros = []
@@ -33,18 +34,20 @@ NonZeros = []
 Rows = []
 
 # The range of legal cols
-Cols = []
+Cols = xrange(10000, 100000)
 
 # Legal values for symmetry
-Sym = ['yes']
+Sym = []
 
 # Legal values for positive definite
-Spd = ['yes']
+Spd = []
 
 def ToInt(stringVal):
     return int(stringVal.replace(',', ''))
 
-def ShouldDownload(group, name, rows, cols, nonZeros, spd, sym):
+def ShouldDownload(group, name, rows, cols, nonZeros, spd, sym, valuetype):
+    if valuetype != ValueType:
+        return False
     if Rows and rows not in Rows:
         return False
     if Cols and cols not in Cols:
@@ -53,23 +56,20 @@ def ShouldDownload(group, name, rows, cols, nonZeros, spd, sym):
         return False
     if Sym and not sym in Sym:
         return False
-    if MAX_COL_SIZE and cols > MAX_COL_SIZE:
-        return False
-    if MAX_ROW_SIZE and rows > MAX_ROW_SIZE:
-        return False
     if MAX_NON_ZEROS and nonZeros > MAX_NON_ZEROS:
         return False
     return True
 
 class MyHtmlParser(HTMLParser):
 
-    def __init__(self):
+    def __init__(self, dryrun):
         HTMLParser.__init__(self)
         self.state = 'NONE'
         self.skipped_header = False
         self.value_fields = []
         self.downloaded_matrices = 0
         self.matrices = []
+        self.dryrun = dryrun
 
     def handle_starttag(self, tag, attrs):
         if self.state == 'FINISHED':
@@ -96,7 +96,6 @@ class MyHtmlParser(HTMLParser):
             self.handle_matrix_entry()
             self.value_fields = []
 
-
     def handle_data(self, data):
         if self.state == 'FINISHED':
             return
@@ -114,30 +113,46 @@ class MyHtmlParser(HTMLParser):
         rows = ToInt(fields[6])
         cols = ToInt(fields[7])
         nonZeros = ToInt(fields[8])
+        valuetype = fields[9].split()[0]
         spd = fields[10]
         sym = fields[11]
 
-        if ShouldDownload(group, name, rows, cols, nonZeros, spd, sym):
+        if ShouldDownload(group, name, rows, cols, nonZeros, spd, sym, valuetype):
             url = 'http://www.cise.ufl.edu/research/sparse/MM/' + group + '/' + name + '.tar.gz'
 
-            print 'Fetching matrix: ' + group + ' ' + name
-            filename = wget.download(url)
-            print '... Done'
+            print 'Fetching matrix: ' + group + ' ' + name, valuetype
+
+            filename = None
+            if not self.dryrun:
+                filename = wget.download(url)
+                print '... Done'
             self.downloaded_matrices += 1
-            self.matrices.append(Matrix(group, name, matrixId, rows, cols, nonZeros, filename))
+            self.matrices.append(Matrix(group, name, matrixId,
+                                        rows, cols, nonZeros, filename,
+                                        valuetype
+                                    ))
             if self.downloaded_matrices >= MATRIX_LIMIT:
                 self.state = 'FINISHED'
+
+        if group == 'Springer':
+            print group, name, valuetype
 
     def GetDownloadedMatrices(self):
         return self.matrices
 
+
 def RunBenchmark(matrix):
     pass
 
+
 def main():
-    # if len(sys.argv) != 2:
-    #     print "Usage: python get_matrices.py <html_file>"
-    #     return
+    parser = argparse.ArgumentParser(
+        description='Download sparse matrices from UoF Collection.')
+    parser.add_argument('-n', '--dryrun',
+                        action='store_true',
+                        default=False,
+                        help='Print matrices that would be downloaded.')
+    args = parser.parse_args()
 
     print 'Fetching matrix list...'
     url = 'http://www.cise.ufl.edu/research/sparse/matrices/list_by_nnz.html'
@@ -147,19 +162,21 @@ def main():
     filename = 'list_by_nnz.html'
     f = open(filename)
 
-    parser = MyHtmlParser()
+    parser = MyHtmlParser(args.dryrun)
     parser.feed(f.read())
 
     matrices = parser.GetDownloadedMatrices()
 
+    # print matrices
     print 'Fetched {} matrices: '.format(len(matrices))
-    print matrices
 
     # prepare matrices (move to directory, extract archive...)
     shutil.rmtree('matrices', True)
     os.mkdir('matrices')
 
     for matrix in matrices:
+        if not matrix.file:
+            continue
         print matrix.file
         shutil.move(matrix.file, 'matrices')
         call(['tar','-xvzf', 'matrices/' + matrix.file, '-C', 'matrices/'])
