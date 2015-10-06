70f09fabc897dde672ab1972bca3914778d07b31
Paul Grigoras <paul.grigoras90@gmail.com>
2015-10-06 15:32:23 +0100

    Add an example unit test

---
+++ unit-tests/spmv_test.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE spmv_test
+#include <boost/test/unit_test.hpp>
+
+#include <Spark/Spmv.hpp>
+
+BOOST_AUTO_TEST_CASE(logic_resource_usage_test)
+{
+  spark::spmv::LogicResourceUsage ru{1, 1, 1, 1};
+  BOOST_CHECK_EQUAL(ru.luts, 1);
+}
