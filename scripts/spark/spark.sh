39022730befa1cd87b0bd675b7181a06e415b6b4
Paul Grigoras <paul.grigoras90@gmail.com>
2015-10-22 12:23:21 +0100

    Update file path

--- scripts/spark/spark.sh
+++ scripts/spark/spark.sh
@@ -3,20 +3,22 @@
 
 # Run DSE
 
-FILE=../test_out.json
+DSE_FILE=../../test_out.json
 ARCH_CFG=architecture_config.out
+BUILD_DIR=../../src/spmv/build build
 
 # 1. Run DSE tool
+# TODO
 
 # 2. Convert DSE output to a list of MAX_BUILDPARAMS
-sed -e '/"architecture_params"/,/}/!d' ${FILE} | xargs -n9 \
+sed -e '/"architecture_params"/,/}/!d' ${DSE_FILE} | xargs -n9 \
  | sed -e 's/architecture_params: //g' -e 's/{//g' -e 's/},//g' \
  | sed -e 's/:/=/g' -e 's/ //g' | sed -e 's/,/ /g' > ${ARCH_CFG}
 
 # 3. Start a build
 while read p; do
   echo "Starting build $p"
-  MAX_BUILDPARAMS="$p" make -C ../src/spmv/build build
+  MAX_BUILDPARAMS="$p" make -C ${BUILD_DIR}
 done < ${ARCH_CFG}
 
 echo $results
