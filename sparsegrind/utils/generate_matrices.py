53af8a63160e774748f961c8b0116dd94419f46e
Paul Grigoras <paul.grigoras90@gmail.com>
2015-11-05 12:41:57 +0000

    Update gen matrices script

--- sparsegrind/utils/generate_matrices.py
+++ sparsegrind/utils/generate_matrices.py
@@ -5,15 +5,19 @@ def print_header():
    print '%%MatrixMarket matrix coordinate real general'
 
 def main():
-  order     = int(sys.argv[1])
-  rowLength = order
-  cols = order
+  if len(sys.argv) < 3:
+    print 'usage: generate_matrices rows cols elemsPerRow'
+    sys.exit(1)
+
+  rows = int(sys.argv[1])
+  cols = int(sys.argv[2])
+  elemsPerRow = int(sys.argv[3])
 
   print_header()
-  print rowLength, cols, rowLength * cols
+  print rows, cols, rows * elemsPerRow
 
-  for i in range(1, rowLength + 1):
-    for j in range(1, cols + 1):
+  for i in range(1, rows + 1):
+    for j in range(1, elemsPerRow + 1):
       value = random.random()
       print i, j, value
 
