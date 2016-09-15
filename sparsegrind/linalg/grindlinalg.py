2a72a721f44fa3268270966df2793cf05a1335da
Paul Grigoras <paul.grigoras90@gmail.com>
2016-09-15 23:48:46 +0100

    Support running linalg from external modules

--- sparsegrind/linalg/grindlinalg.py
+++ sparsegrind/linalg/grindlinalg.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 import sys
 from termcolor import colored
+from tabulate import tabulate
 
 import scipy.sparse.linalg as spla
 
@@ -18,8 +19,11 @@ def runSolver(solver, system, rhs, squeeze=True):
   check = system.dot(sol).transpose()
   print np.linalg.norm(np.subtract(got, check))
 
+def l2norm(exp, got):
+    return np.linalg.norm(np.subtract(got, exp))
+
 def check(msg, exp, got):
-  print msg, 'L2 norm:', np.linalg.norm(np.subtract(got, exp))
+  print msg, 'L2 norm:', l2norm(got, exp)
 
 class SolverInfo(object):
   def __init__(self):
@@ -29,19 +33,80 @@ class SolverInfo(object):
     # print x
     self.iterations += 1
 
+
+def runCgSolvers(matrix_path, vec_path, use_precon=True, max_size=None):
+  system = sparse.csr_matrix(io.mmread(matrix_path))
+  rhs = io.mmread(vec_path)
+  n = system.shape[0]
+  if max_size and n > max_size:
+    print colored('System too large ignoring')
+    sys.exit(1)
+  print colored('Testing system ' + matrix_path, 'red')
+  print io.mminfo(matrix_path), io.mminfo(vec_path), float(system.nnz) / n
+  # -- Direct solver
+  t0 = time.time()
+  dir_sol = scipy.sparse.linalg.spsolve(system, rhs)
+  t1 = time.time()
+  directSolveTime = t1 - t0
+  if type(rhs) is not np.ndarray:
+    rhs = rhs.toarray()
+  check('Direct Solver', np.squeeze(rhs), system.dot(dir_sol))
+  # -- CG Solver
+  resultsList = []
+  for solver in [spla.bicgstab]:  # , spla.cg, spla.gmres]: #, spla.bicg, spla.cgs, spla.minres, spla.qmr]:
+    # We can try these out in higher ranges, but for now it seems that ideal values
+    # are around 100 for the fill_factor and 1e-12 for the drop tollerance
+    for fill_factor in [10, 100]:  # 10000, 1000, 100, 10]:
+      for drop_tol in [1e-6, 1e-12]:  # , 1e-8, 1e-6]:
+        t0 = time.time()
+        approx_inv = scipy.sparse.linalg.spilu(
+          sparse.csc_matrix(system),
+          fill_factor=fill_factor,
+          drop_tol=drop_tol)
+        t1 = time.time()
+        preconTime = t1 - t0
+        # print approx_inv
+        M = scipy.sparse.linalg.LinearOperator(
+          (n, n), lambda x: approx_inv.solve(x))
+        # M = scipy.sparse.linalg.inv(approx_inv)
+
+        precon = use_precon and solver not in [spla.bicg, spla.minres, spla.qmr]
+        solverInfo = SolverInfo()
+        t0 = time.time()
+        if precon:
+          sol = solver(system, rhs, M=M, maxiter=2000, callback=solverInfo.printer)
+        else:
+          sol = solver(system, rhs, maxiter=n)
+        t1 = time.time()
+        iterativeSolverTime = t1 - t0
+        result = sol[0]
+        # print 'Direct solution', dir_sol
+        # print 'Iterative', result
+        resultsList.append(
+          [
+            fill_factor, drop_tol, solver, precon, solverInfo.iterations,
+            directSolveTime, preconTime, iterativeSolverTime, preconTime + iterativeSolverTime,
+            l2norm(np.squeeze(rhs), system.dot(result))
+          ]
+        )
+  headers = ['fill_factor', 'drop_tol', 'solver', 'precon', 'iterations', 'direct(s)', 'precon(s)', 'it(s)', 'itTotal(s)', 'l2norm']
+#  print tabulate(resultsList, headers)
+  return resultsList, headers
+
+
 def main():
   parser = argparse.ArgumentParser(
-      description='Benchmark sparse linear solvers',
-      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    description='Benchmark sparse linear solvers',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument('-m', '--matrix-path',
-      help='Path to matrix to use (in Matrix Market Format')
+                      help='Path to matrix to use (in Matrix Market Format')
   parser.add_argument('-b',
-      help='Path to RHS to use (in Matrix Market format)')
+                      help='Path to RHS to use (in Matrix Market format)')
   parser.add_argument('-p', '--precon',
-      default=False, action='store_true',
-      help='Use preconditioner')
+                      default=False, action='store_true',
+                      help='Use preconditioner')
   parser.add_argument('-d', '--directory',
-      help='Path to directory to load vector & matrix from')
+                      help='Path to directory to load vector & matrix from')
   parser.add_argument('-ms', '--maxsize',
                       type=int, help='Maximum system size')
   args = parser.parse_args()
@@ -65,61 +130,8 @@ def main():
     matrix_path = args.matrix_path
     vector_path = args.b
 
-  system = sparse.csr_matrix(io.mmread(matrix_path))
-  rhs = io.mmread(vec_path)
+  runCgSolvers(args, matrix_path, vec_path)
 
-  n = system.shape[0]
-  print args.maxsize
-  if args.maxsize and n > args.maxsize:
-    print colored('System too large ignoring')
-    sys.exit(1)
-
-  print colored('Testing system ' + matrix_path, 'red')
-  print io.mminfo(matrix_path), io.mminfo(vec_path), float(system.nnz) / n
-
-  # -- Direct solver
-
-  t0 = time.time()
-  dir_sol = scipy.sparse.linalg.spsolve(system, rhs)
-  t1 = time.time()
-  directSolveTime = t1 - t0
-  if type(rhs) is not np.ndarray:
-    rhs = rhs.toarray()
-  check('Direct Solver', np.squeeze(rhs), system.dot(dir_sol))
-
-  # -- CG Solver
-  for solver in [spla.bicgstab]: #, spla.cg, spla.gmres]: #, spla.bicg, spla.cgs, spla.minres, spla.qmr]:
-    # We can try these out in higher ranges, but for now it seems that ideal values
-    # are around 100 for the fill_factor and 1e-12 for the drop tollerance
-    for fill_factor in [10, 100]: # 10000, 1000, 100, 10]:
-      for drop_tol in [1e-6, 1e-12]: #, 1e-8, 1e-6]:
-        t0 = time.time()
-        approx_inv = scipy.sparse.linalg.spilu(
-            sparse.csc_matrix(system),
-            fill_factor = fill_factor,
-            drop_tol = drop_tol)
-        t1 = time.time()
-        preconTime = t1 - t0
-        # print approx_inv
-        M = scipy.sparse.linalg.LinearOperator(
-           (n, n), lambda x: approx_inv.solve(x))
-        # M = scipy.sparse.linalg.inv(approx_inv)
-
-        precon = args.precon and solver not in [spla.bicg, spla.minres, spla.qmr]
-        solverInfo = SolverInfo()
-        t0 = time.time()
-        if precon:
-          sol = solver(system, rhs, M=M, maxiter=2000, callback=solverInfo.printer)
-        else:
-          sol = solver(system, rhs, maxiter=n)
-        t1 = time.time()
-        iterativeSolverTime = t1 - t0
-        result = sol[0]
-        # print 'Direct solution', dir_sol
-        # print 'Iterative', result
-        check('fill_factor {:4}, drop_tol {:4}, solver {:40}, precon {}, iterations {}, direct(s) {}, precon()s {} iterative(s) {}'.format(
-          fill_factor, drop_tol, solver, precon, solverInfo.iterations, directSolveTime, preconTime, iterativeSolverTime),
-          np.squeeze(rhs), system.dot(result))
 
 if __name__ == '__main__':
   main()
