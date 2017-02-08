63b19f4fd8f8481ed1c4a623e8855eeb731cc861
Paul Grigoras <paul.grigoras90@gmail.com>
2017-02-08 19:21:45 +0000

    Remove unused code

--- tests/test_cg.cpp
+++ tests/test_cg.cpp
@@ -1,24 +1,3 @@
-// #include <iostream>
-
-// #include <Spark/SparseLinearSolvers.hpp>
-// #include <test_utils.hpp>
-
-// int main()
-// {
-//   int status = -1;
-//   // spark::sparse_linear_solvers::DfeCgSolver solver{};
-//   // status |= test(16, spark::test::IdentityGenerator{}, spark::test::SimpleVectorGenerator{}, solver);
-//   // status |= test(100, spark::test::RandomGenerator{}, spark::test::SimpleVectorGenerator{}, solver);
-//   // status |= spark::test::runTest("../test-matrices/bfwb62.mtx", solver);
-//   //status |= runMatrixVectorTest(
-//       //"../test-matrices/OPF_3754.mtx",
-//       //"../test-matrices/OPF_3754_b.mtx");
-//   //status |= runMatrixVectorTest(
-//       //"../test-matrices/OPF_6000.mtx",
-//       //"../test-matrices/OPF_6000_b.mtx");
-//   return status;
-// }
-
 #include <vector>
 #include <Spark/IO.hpp>
 #include <Spark/Benchmark.hpp>
